1b3c65dae7e94fdcff0c0fa5fb53b7552e96d9b8
Akshay Nair <phenax5@gmail.com>
2016-12-06 22:42:18 +0530

    Added google analytics

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -103,10 +103,24 @@ export default class IndexLayout extends React.Component {
 
 					<script async defer src='/src/dist/js/script.js'></script>
 					<script dangerouslySetInnerHTML={{ __html: this.deferAsyncForLinks }} />
+					<script dangerouslySetInnerHTML={{
+						__html: this.googleAnalytics 
+					}} />
 				</body>
 			</html>
 		);
 	}
+
+
+	googleAnalytics= `
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-88549129-1', 'auto');
+		ga('send', 'pageview');
+	`.replace(/\s+/g, ' ');
 
 
 	deferAsyncForLinks= `
