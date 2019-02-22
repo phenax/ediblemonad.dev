6538e0ecf91256bcf60b8ebfa3957e8ae3148706
Akshay Nair <phenax5@gmail.com>
2019-02-22 23:05:02 +0530

    Helmet changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const AboutPage = () => (
   <div>
+    <Helmet>
+      <title>Hello wow</title>
+    </Helmet>
     ABOUT MOI
   </div>
 );
