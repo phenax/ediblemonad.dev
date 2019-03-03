0602e0e8e7617dda4c206a5262a73a9dd50a43eb
Akshay Nair <phenax5@gmail.com>
2019-03-03 11:08:45 +0530

    Refactors image loading

--- src/hooks/useLazyImage.ts
+++ src/hooks/useLazyImage.ts
@@ -1,8 +1,25 @@
 import { useEffect, useState } from 'react';
 
+import useIntersectionObserver from './useIntersectionObserver';
 import { imageWrapper } from '../helpers/image';
-
 import { FixedImage } from '../types/image';
+
+const noop = () => {};
+
+const loadImage = (src: string) => {
+  const img = new Image();
+  img.src = src;
+  return {
+    promise: new Promise((resolve, reject) => {
+      img.onload = resolve;
+      img.onerror = reject;
+    }),
+    cancel: () => {
+      img.onload = noop;
+      img.onerror = noop;
+    },
+  };
+};
 
 // TODO: Use intersection observer to trigger load
 const useLazyImage = (image: FixedImage) => {
@@ -11,12 +28,17 @@ const useLazyImage = (image: FixedImage) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<string|Event|null>(null);
 
+  // const intersection = useIntersectionObserver();
+
   useEffect(() => {
     if (isLoaded) return;
-    const img = new Image();
-    img.src = src;
-    img.onload = () => setIsLoaded(true);
-    img.onerror = setError;
+
+    const img = loadImage(src);
+    img.promise
+      .then(() => setIsLoaded(true))
+      .catch(setError);
+
+    return () => img.cancel();
   }, [src]);
 
   return { error, src: isLoaded ? src : base64 };
