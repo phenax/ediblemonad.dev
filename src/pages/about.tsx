c737315e4b84c9ca731a86529649afe4da7aae4f
Akshay Nair <phenax5@gmail.com>
2020-09-06 21:19:23 +0530

    About me text change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,7 +7,9 @@ import s from '../styles/page.module.scss';
 
 const AboutPage = () => (
   <PageLayout title="About me" headerProps={{ title: 'About Me', subtitle: 'type Akshay = Developer | Metalhead | Geek | Nerd;' }}>
-    <h2 className={s.title}>print "Hello world";</h2>
+    <h2 className={s.title}>
+      putStrLn {`"`}Hello world{`"`};
+    </h2>
 
     <p className={s.para}>
       I am Akshay, a full-stack web developer with an obsession for writing functional, maintainable, performant code. I live in Mumbai,
@@ -15,17 +17,18 @@ const AboutPage = () => (
     </p>
 
     <p className={s.para}>
-      Functional programmer to the core, I like working with and building composable apis for every line of code. For the front-end, I enjoy
-      building things with libraries like React, HyperHTML, RxJS, crocks. I build optimized and scalable back-ends with technologies like
-      NodeJS(ExpressJS, PlasmaJS, KoaJS), Kotlin(Vert.X), Golang, Python(flask, japronto, sanic) and PHP(CakePHP).
+      Functional programmer to the core, I like working with and building software that are written as small building blocks that compose
+      together to form a usable application.
     </p>
 
     <p className={s.para}>
-      Also, I love music. A lot! Some of my favorite bands at the time of writing this are Amon Amarth, Avatar, Gojira, Trivium, Killswitch
-      Engage, August Burns Red, etc.{' '}
+      Also, I love music. A lot! Some of my favorite artists at the time of writing this are Amon Amarth, Tool, Polyphia, Gojira, Chon,
+      Killswitch Engage, August Burns Red, Foo Fighters, etc.{' '}
+      {/*
       <Link to="/suggest-music" className={s.link}>
         Tell me about your favorites
       </Link>
+      */}
     </p>
   </PageLayout>
 );
