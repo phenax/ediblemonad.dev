005be1c4dd5165302bab6824be8087d0c8e5f81c
Akshay Nair <phenax5@gmail.com>
2020-09-06 23:57:06 +0530

    Menu styles fixed

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -35,52 +35,9 @@ const items = [
   },
 ];
 
-const Menu = () => {
-  const [isOpen, setMenuState] = useState(false);
-  const { file: { childImageSharp: { fixed: logoImg = { src: '' } } = {} } = {} } = useStaticQuery(query);
-
-  const onMenuToggle = (nextState: boolean) => (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault();
-    setMenuState(nextState);
-  };
-
-  return (
-    <div>
-      <div className={styles.topbar}>
-        <div className={styles.topbarMenubtn}>
-          <a href='#menu' className='fas fa-bars' onClick={onMenuToggle(true)} />
-        </div>
-      </div>
-      <div className={cx(styles.menu, rootStyles.row, { [styles.menu_visible]: isOpen })} id='menu'>
-        <div className={cx(styles.menuSection, styles.menuSide, rootStyles.hideOnSm, rootStyles.col)}>
-          <div className={styles.logo}>
-            <img className={styles.logoImg} draggable={false} src={getImageSrc(logoImg)} />
-            <div className={styles.logoText}>Hey There</div>
-          </div>
-        </div>
-
-        <ul className={cx(styles.menuSection, styles.menuList, rootStyles.col)}>
-          <a href='#' className={cx(styles.menuClosebtn, 'fas fa-times')} onClick={onMenuToggle(false)}></a>
-
-          {items.map(
-            item => (
-              <li className={cx(styles.menuListLi)} key={item.url}>
-                <Link to={item.url} className={cx(styles.menuListLink)}>
-                  <div className={styles.title}>{item.title}</div>
-                  <div className={styles.descr}>{item.descr}</div>
-                </Link>
-              </li>
-            )
-          )}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
 export const query = graphql`
   query MenuData {
-    file(relativePath: {eq: "images/logo/logo.png"}) {
+    file(relativePath: { eq: "images/logo/logo.png" }) {
       childImageSharp {
         fixed(quality: 80, width: 340) {
           src
@@ -92,5 +49,47 @@ export const query = graphql`
     }
   }
 `;
+
+const Menu = () => {
+  const [isOpen, setMenuState] = useState(false);
+  const { pathname } = window.location;
+  const { file: { childImageSharp: { fixed: logoImg = { src: '' } } = {} } = {} } = useStaticQuery(query);
+
+  const onMenuToggle = (nextState: boolean) => (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    setMenuState(nextState);
+  };
+
+  return (
+    <div>
+      <div className={styles.topbar}>
+        <div className={styles.topbarMenubtn}>
+          <a href="#menu" className="fas fa-bars" onClick={onMenuToggle(true)} />
+        </div>
+      </div>
+      <div className={cx(styles.menu, rootStyles.row, { [styles.menu_visible]: isOpen })} id="menu">
+        <div className={cx(styles.menuSection, styles.menuSide, rootStyles.hideOnSm, rootStyles.col)}>
+          <div className={styles.logo}>
+            <img className={styles.logoImg} draggable={false} src={getImageSrc(logoImg)} alt="Akshay Nair" />
+            <div className={styles.logoText}>Hey There</div>
+          </div>
+        </div>
+
+        <ul className={cx(styles.menuSection, styles.menuList, rootStyles.col)}>
+          <a href="#" className={cx(styles.menuClosebtn, 'fas fa-times')} onClick={onMenuToggle(false)} />
+
+          {items.map(item => (
+            <li className={cx(styles.menuListLi, { [styles.menuListLi_selected]: pathname === item.url })} key={item.url}>
+              <Link to={item.url} className={cx(styles.menuListLink)}>
+                <div className={styles.title}>{item.title}</div>
+                <div className={styles.descr}>{item.descr}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
