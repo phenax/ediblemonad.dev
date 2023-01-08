d9c95872386fb6c007cbc13e4d14a0c34cc6eddd
Akshay Nair <phenax5@gmail.com>
2023-01-08 15:05:20 +0530

    feat: adds contacts page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,16 +5,17 @@ export default function About() {
   return (
     <main className={styles.main}>
       <h2 className="text-2xl text-bold pb-6" style={{ color: 'rgb(var(--accent-rgb))' }}>
-        {`console.log("Hello, there!")`}
+        {`> console.log("Hello, there!")`}
       </h2>
 
       <div className={`indent-6 text-slate-400 ${styles.paraWrap}`}>
         <p>My name is <b>Akshay Nair</b> (as the giant header above might have given away). I am a <b>full-stack web developer</b> from Mumbai, India (+5:30 GMT).</p>
 
-        <p>I have professional experience working full-stack on a variety of projects from a <b>matrimonial website</b> to a <b>video editor</b> on the web to a <b>realtime electronic lab notebook</b>. I also have a lot more experience working on <b>personal projects</b> and contributing to <b>open source</b></p>
+        <p>I have 6+ years of professional experience working full-stack on a variety of projects from a <b>matrimonial website</b> to a <b>video editor</b> on the web to a <b>realtime electronic lab notebook</b>. In addition to that, I also have a lot more experience working on <b>personal projects</b> and contributing to <b>open source</b>.</p>
 
         <p>
-          In my free time I like to work on personal projects live on <b>twitch</b> (<a href="https://twitch.tv/EdibleMonad" target="_blank _parent" className={commonStyles.link}>@EdibleMonad</a>)
+          I also like to dabble in some <b>esoteric</b> coding ideas, <b>creative coding</b>, etc in my free time.
+          {` I've`} also taken up live-coding my personal projects on <b>twitch</b> (<a href="https://twitch.tv/EdibleMonad" target="_blank _parent" className={commonStyles.link}>@EdibleMonad</a>)
         </p>
       </div>
     </main>
---
+++ app/contact/page.tsx
@@ -0,0 +1,68 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import styles from '../page.module.css'
+import commonStyles from '../common.module.css'
+
+config.autoAddCss = false;
+
+const contacts = [
+  {
+    link: 'https://github.com/phenax',
+    icon: faGithub,
+    text: 'Github',
+    brandColor: '#ffffff',
+  },
+  {
+    link: 'https://www.linkedin.com/in/akshay-nair5',
+    icon: faLinkedin,
+    text: 'LinkedIn',
+    brandColor: '#006192',
+  },
+  {
+    link: 'https://twitch.tv/EdibleMonad',
+    icon: faTwitch,
+    text: 'Twitch',
+    brandColor: '#6441A4',
+  },
+  {
+    link: 'https://twitter.com/EdibleMonad',
+    icon: faTwitter,
+    text: 'Twitter',
+    brandColor: '#55ACEE',
+  },
+]
+
+const email = 'akshay.n0@protonmail.com'
+
+export default function Contact() {
+  return (
+    <main className={styles.main}>
+      <h2 className="text-2xl text-bold mt-3 pb-6 text-center">
+        <a href={`mailto:${email}`} className={commonStyles.link} target="_blank _parent">{email}</a>
+      </h2>
+
+      <div className="flex w-full justify-around mt-6">
+        {contacts.map(c => (
+          <a href={c.link} target="_blank _parent" className={`block ${styles.contactLink} ${commonStyles.link}`} key={c.link}>
+            <div className="px-4 py-6 text-center">
+              <span className="inline-block relative">
+                <FontAwesomeIcon
+                  icon={c.icon}
+                  className={`text-4xl pb-6 ${styles.contactLinkIcon}`}
+                />
+                <FontAwesomeIcon
+                  icon={c.icon}
+                  className={`text-4xl pb-6 ${styles.contactLinkBrandIcon}`}
+                  style={{ color: c.brandColor }}
+                />
+              </span>
+              <div>{c.text}</div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </main>
+  )
+}
