41c82bbdec7c9ffb1edffbb6264a7b7b55da1900
Akshay Nair <phenax5@gmail.com>
2024-06-01 16:21:53 +0530

    Add yaml-transformer to projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const projectIds: Array<keyof typeof projectData> = [
   'edible-desmos',
   'typed-regex',
   'ts-typeslang',
+  'yaml-transformer',
   'algebraic-effects',
   'diary-pwa',
   'pattern-lock-js',
