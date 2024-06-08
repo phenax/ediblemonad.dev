0dba55fa1a124ffc20c4720ee74673175f625d14
Akshay Nair <phenax5@gmail.com>
2024-06-08 13:25:29 +0530

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,7 @@ export default function AppLayout({ Component, pageProps }: any) {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
         <title>Projects - Akshay Nair</title>
         <meta name="description" content="List of projects made by Akshay Nair, who is a full-stack web developer based in India" />
